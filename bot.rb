e8a5430d243d9ebee77d7d6eeb87d584304a187f
isarceaux <isarceaux>
2017-07-12 14:32:42 +0200

    Premier test de récupération des tweets qui fonctionne après configuration des variables d'environnement réussie

---
+++ bot.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require "rubygems"
+require "twitter"
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+end
+
+client.search("#ruby").take(50).each do |tweet|
+  puts tweet.text
+end
